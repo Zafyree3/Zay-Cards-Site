c4dff4cb112723b54b5c44a585ee6ffd6084b06b
Irman Zaf <irman_zafyree_bin_irmanizam@s2019.ssts.edu.sg>
2023-06-09 02:34:21 +0800

    MASSIVE UPDATE

--- app/page.js
+++ app/page.js
@@ -2,15 +2,31 @@
 
 import { useEffect, useState, useRef } from "react";
 import Axios from "axios";
+import './globals.css'
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import { Popover, RadioGroup  } from '@headlessui/react'
 
 export default function Home() {
+
+  const themes = [
+    {
+      name: "Blue",
+      value: "theme-blue",
+    },
+    { 
+      name: "Dark",
+      value: "theme-dark",
+    }
+  ]
+
   const [data, setData] = useState([]);
   const [cardDetails, setCardDetails] = useState([]);
   const [amountLoaded, setAmountLoaded] = useState(10);
   const [isLastCardIntersecting, setIsLastCardIntersecting] = useState(false);
-  const [allCardsLoaded, setAllCardsLoaded] = useState(false);
+  const [theme, setTheme] = useState(localStorage.getItem("theme") || "theme-blue");
 
   const observee = useRef(null);
+  const mainBody = useRef(null);
 
   const API_PATH = process.env.API_URL;
 
@@ -33,12 +49,9 @@ export default function Home() {
       });
   };
 
-  const getCardDetails = async (id) => {
-
-    const response = await Axios.get( API_PATH + "/GetCardDetails", {
-      params: {
-        id: id
-      }
+  const getCardDetailsBatch = async (ids) => {
+    const response = await Axios.post( API_PATH + "/GetCardDetailsBatch", {
+      ids: [...ids]
     })
     if (response.status === 200) {
       return response.data;
@@ -46,6 +59,12 @@ export default function Home() {
       console.error(response);
     }
   };
+
+  const setWebsiteTheme = (theme) => {
+    let classes = mainBody.current.classList.value
+    classes = classes.replace(/theme-(?:\w*)/, theme)
+    mainBody.current.classList = classes
+  }
 
   useEffect(() => { // Get data from Notion
     getNotionData(0, amountLoaded);
@@ -56,22 +75,20 @@ export default function Home() {
       return;
     }
 
-    if (allCardsLoaded) { // Check all cards are loaded
-      return;
-    }
-
-    const gettingDetails = async () => {
+    const gettingDetailsByBatch = async () => {
       let allCardDetails = [...cardDetails];
-      for (let i = allCardDetails.length; i < data.length; i++) {
-        const details = await getCardDetails(data[i]["Card ID"]);
-        details["Stock"] = data[i]["Stock"];
-        allCardDetails.push(details);
+      const ids = data.slice(allCardDetails.length, data.length).map(card => card["Card ID"]);
+      const details = await getCardDetailsBatch(ids);
+      for (let i = 0; i < details.length; i++) {
+        details[i]["Stock"] = data[i]["Stock"];
       }
+      allCardDetails.push(...details);
 
       setCardDetails(allCardDetails);
+
     }
 
-    gettingDetails();
+    gettingDetailsByBatch();
   }, [data]);
 
   useEffect(() => { // Observe last card
@@ -106,23 +123,60 @@ export default function Home() {
     }
   }, [isLastCardIntersecting]);
 
-  // useEffect(() => { // Check all cards are loaded
+  useEffect(() => { // Get theme from local storage
+    const theme = localStorage.getItem("theme");
+    if (theme === null) {
+      localStorage.setItem("theme", "theme-blue");
+    } else {
+      // mainBody.current.classList.replace(RegExp('/(?:^|\s)theme-(?:\w*)'), theme);
+      setWebsiteTheme(theme);
+    }
+  }, []);
 
-  //   if (cardDetails.length !== 0 && data.length !== 0 && data.length < amountLoaded) {
-
-  //     console.log("All cards loaded");
-  //     setAllCardsLoaded(true);
-  //   }
-
-  // }, [cardDetails]);
-
+  useEffect(() => { // Set theme to local storage
+    localStorage.setItem("theme", theme);
+    setWebsiteTheme(theme);
+  }, [theme]);
   
   return (
-    <body className="w-full flex flex-col items-center bg-white">
-      <header className="w-full h-28 bg-gray-800 flex flex-row justify-center items-center">
+    <body ref={mainBody} className="w-full flex flex-col items-center bg-background theme-blue">
+      <header className="w-full h-28 bg-primary flex flex-row justify-center items-center relative z-10">
+        <div id="control-panel" className="w-fit h-fit absolute top-1/2 -translate-y-1/2 left-1/24 flex gap-8">
+          <Popover className="relative">
+            <Popover.Button className="aspect-square p-2 rounded-md outline outline-1 outline-white">
+              <i className="bi bi-palette2 inline-block text-2xl/[0] text-white"></i>
+            </Popover.Button>
+            <Popover.Panel className="absolute w-1/7vw bg-white p-3 shadow-popup mt-3 rounded-md">
+              <RadioGroup value={theme} onChange={setTheme} className="flex flex-col gap-5">
+                <RadioGroup.Label>Themes</RadioGroup.Label>
+                {themes.map((theme) => (
+                  <RadioGroup.Option key={theme.name} value={theme.value}
+                    className={`${theme.value} py-1 px-3 h-1/15vh bg-background outline outline-2 outline-primary outline-offset-1 rounded-sm flex flex-row`}>
+                        {({ checked }) => (
+                          <>
+                            <div className="flex flex-col w-2/3 h-full items-start justify-center ">
+                              <p className="text-primary font-bold text-lg">Primary</p>
+                              <p className="text-text font-light text-sm">Text</p>
+                            </div>
+                            <div className="w-1 h-full bg-secondary"></div>
+                            <div className="flex flex-1 justify-center items-center">
+                              {checked ? <i className="bi bi-circle-fill text-2xl/[0] text-primary"></i> : <i className="bi bi-circle text-2xl/[0] text-primary"></i>}
+                            </div>
+                            
+                          </>
+                        )}
+                    </RadioGroup.Option>
+                ))}
+              </RadioGroup>
+            </Popover.Panel>
+          </Popover>
+          <button className="aspect-square p-2 rounded-md outline outline-1 outline-white">
+            <i className="bi bi-funnel-fill inline-block text-2xl/[0] text-white"></i>
+          </button>
+        </div>
         <h1 className="text-white text-5xl font-bold">Card Shop</h1>
       </header>
-      <main className="w-11/12 h-fit grid grid-cols-6 grid-rows-5 py-3 gap-5">
+      <main className="w-11/12 h-fit grid grid-cols-6 grid-rows-5 py-3 gap-5 z-0">
         {data.map((_, index) => {
           return (
             index === amountLoaded - 1 ?
@@ -145,7 +199,7 @@ function Card({ cardDetails }) {
   let cardID = cardDetails !== undefined ? cardDetails["ID"] : null;
   let cardCode = cardDetails !== undefined ? cardDetails["Code"] : null;
   let SetNumber = cardDetails !== undefined ? cardDetails["SetNumber"] : null;
-  let animeName = cardDetails !== undefined ? cardDetails["Series"] : null;
+  let animeName = cardDetails !== undefined ? cardDetails["SeriesName"] : null;
 
 
 
@@ -160,11 +214,19 @@ function Card({ cardDetails }) {
     //   </div>
     // </main>
 
-    <main className="w-full aspect-potrait relative bg-white shadow-card p-2 flex flex-col rounded">
-      {image === null ? <div className="aspect-card w-full z-0 bg-grey-200"></div> : <img src={image} alt={name} className="aspect-card w-full z-0 object-cover" loading="lazy"/> } 
+    <main className="w-full aspect-potrait relative bg-white shadow-card pt-5 pr-5 pl-5 pb-5 flex flex-col rounded drop-shadow-card gap-2.5 overflow-hidden">
+      {image === null ? <div className="aspect-card w-full z-0 bg-gray-200"></div> : <img src={image} alt={name} className="aspect-card w-full z-0 object-cover" loading="lazy"/> } 
       <div className="flex-1 w-full flex flex-col justify-center items-center">
-        <p className="text-base">
+        <p className="w-full h-fit text-md text-text font-bold text-center text-ellipsis">
           {name}
+        </p>
+        <p className="w-full h-fit text-sm text-secondary font-light text-center text-ellipsis">
+          {animeName}
+        </p>
+      </div>
+      <div className="absolute w-full h-1/9 bg-gold/[0.9] flex justify-center items-center top-8 left-rarity -rotate-45">
+        <p className="text-white text-3xl font-bold">
+          {rarity}
         </p>
       </div>
     </main>
--- tailwind.config.js
+++ tailwind.config.js
@@ -16,7 +16,30 @@ module.exports = {
         "potrait": "9/16"
       },
       boxShadow: {
-        "card": '0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23)'
+        "card": "0 5px 5px rgba(0,0,0,0.3)",
+        "popup" : "rgba(0, 0, 0, 0.19) 0px 10px 20px, rgba(0, 0, 0, 0.23) 0px 6px 6px",
+      },
+      fontSize: {
+        "md": ["1.0625rem", "1.625rem"],
+      }, 
+      width: {
+        "9/20": "45%",
+        "1/7vw" : "calc(100vw / 7)",
+      },
+      height: {
+        "1/9": "11.1111111111%",
+        "1/15vh" : "calc(100vh / 16)",
+      },
+      colors: {
+        "primary" : "var(--_color-primary)",
+        "background" : "var(--_color-background)",
+        "text" : "var(--_color-text)",
+        "secondary" : "var(--_color-secondary)",
+        "gold" : "#d4af37",
+      },
+      inset : {
+        "rarity": "calc(-50% + 2.5rem)",
+        "1/24" : "4.1666666667%",
       }
     },
   plugins: [],
